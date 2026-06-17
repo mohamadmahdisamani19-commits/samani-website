26644ff9a54c7b5368c487320e5f6bbd78a016b4
mohamadmahdisamani19-commits <mohamadmahdisamani19@gmail.com>
2026-06-17 10:45:04 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname)));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'samani_shop-3.html')));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Port ${PORT}`));
